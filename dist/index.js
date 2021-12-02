aa20cd888e15f6e7e06bef734b8a8c00a3b8b8c4
Jacob Wejendorp <jwe@tradeshift.com>
2021-12-02 03:25:22 +0100

    feat: support base64 encoded certs

--- dist/index.js
+++ dist/index.js
@@ -99,14 +99,21 @@ const core_1 = __nccwpck_require__(186);
 const inputs_1 = __nccwpck_require__(180);
 const upload = __importStar(__nccwpck_require__(831));
 const fs = __importStar(__nccwpck_require__(747));
+function processCert(cert) {
+    const isBase64Exp = /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$/;
+    if (isBase64Exp.exec(cert)) {
+        return Buffer.from(cert, "base64").toString("utf-8");
+    }
+    return cert;
+}
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const inputs = yield inputs_1.getInputs();
             if (inputs.ca !== undefined && inputs.ca.length > 0) {
-                fs.writeFileSync("/tmp/ca.pem", inputs.ca);
-                fs.writeFileSync("/tmp/cert.pem", inputs.cert);
-                fs.writeFileSync("/tmp/key.pem", inputs.key);
+                fs.writeFileSync("/tmp/ca.pem", processCert(inputs.ca));
+                fs.writeFileSync("/tmp/cert.pem", processCert(inputs.cert));
+                fs.writeFileSync("/tmp/key.pem", processCert(inputs.key));
             }
             yield upload.run(inputs);
         }
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,23 @@ import { setFailed } from "@actions/core";
 import { getInputs } from "./inputs";
 import * as upload from "./upload";
 import * as fs from "fs";
+
+function processCert(cert: string) {
+  const isBase64Exp =
+    /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$/;
+  if (isBase64Exp.exec(cert)) {
+    return Buffer.from(cert, "base64").toString("utf-8");
+  }
+  return cert;
+}
+
 async function run(): Promise<void> {
   try {
     const inputs = await getInputs();
     if (inputs.ca !== undefined && inputs.ca.length > 0) {
-      fs.writeFileSync("/tmp/ca.pem", inputs.ca);
-      fs.writeFileSync("/tmp/cert.pem", inputs.cert);
-      fs.writeFileSync("/tmp/key.pem", inputs.key);
+      fs.writeFileSync("/tmp/ca.pem", processCert(inputs.ca));
+      fs.writeFileSync("/tmp/cert.pem", processCert(inputs.cert));
+      fs.writeFileSync("/tmp/key.pem", processCert(inputs.key));
     }
 
     await upload.run(inputs);
